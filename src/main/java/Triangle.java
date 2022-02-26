50b98364d0804527d8ec8c2573eba4f057bfdcb3
dbc2201 <divyanshb22@gmail.com>
2022-02-26 20:51:29 +0530

    initialize the private fields inside the constructor

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -5,6 +5,9 @@ class Triangle {
     private final double side3;
 
     Triangle(double side1, double side2, double side3) throws TriangleException {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 
