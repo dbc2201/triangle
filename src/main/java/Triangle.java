8d116307d1e8e5d2da1d980e0b3f3288910d5cf9
dbc2201 <divyanshb22@gmail.com>
2022-02-26 20:50:48 +0530

    add fields for the triangle class

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,5 +1,9 @@
 class Triangle {
 
+    private final double side1;
+    private final double side2;
+    private final double side3;
+
     Triangle(double side1, double side2, double side3) throws TriangleException {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
